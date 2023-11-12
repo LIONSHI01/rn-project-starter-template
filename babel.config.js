e8add21b9ee59a8a16a47cf8f31f36baae2ce510
lionc <shizijian2010@yahoo.com.hk>
2023-11-12 17:47:52 +0800

    add absolute import config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,21 @@
 // Specify app folder location is inside /src
-process.env.EXPO_ROUTER_APP_ROOT = 'src/app';
+// process.env.EXPO_ROUTER_APP_ROOT = 'src/app';
 
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@src': './src',
+          },
+        },
+      ],
+    ],
   };
 };
